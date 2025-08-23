0ff97a385273ea3121fe892f5d516c05ecee0229
kana <kana>
2025-08-23 21:34:14 +0500

    fix bracketss

--- functions/brackets.go
+++ functions/brackets.go
@@ -1,19 +1,12 @@
 package reloaded
 
-import "fmt"
-
 func BracketsFix(arr []string) []string {
-	//for k := 0; k < len(arr); k++ {
-	//	fmt.Println(arr[k])
-	//
-	//}
-
-	fmt.Println(arr)
 	counter := 0
+
 	for i := 0; i < len(arr); i++ {
 		if arr[i] == "(" || arr[i] == "[" || arr[i] == "{" {
 			for j := i + 1; ; {
-				if arr[j] == "\n" {
+				if arr[j] == "\n" || IsCommand(arr[j]) {
 					counter++
 					j++
 				} else {
@@ -22,24 +15,15 @@ func BracketsFix(arr []string) []string {
 			}
 			arr[i] = arr[i] + arr[i+counter+1]
 			arr = append(arr[:i+1], arr[i+counter+2:]...)
-			//i--
+			i--
 		}
 
 		counter = 0
-
 	}
-	//fmt.Println("BracketsFix =", arr)
-	//
-	fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++")
-	//for k := 0; k < len(arr); k++ {
-	//	fmt.Println(arr[k])
-	//
-	//}
-	fmt.Println(arr)
 
-	for l := 0; l < len(arr); l++ {
-		if arr[l] == ")" || arr[l] == "]" || arr[l] == "}" {
-			for v := l - 1; ; {
+	for k := 0; k < len(arr); k++ {
+		if arr[k] == ")" || arr[k] == "]" || arr[k] == "}" {
+			for v := k - 1; ; {
 				if arr[v] == "\n" || IsCommand(arr[v]) {
 					counter++
 					v--
@@ -47,15 +31,11 @@ func BracketsFix(arr []string) []string {
 					break
 				}
 			}
-			arr[l-counter-1] = arr[l-counter-1] + arr[l]
-			arr = append(arr[:l], arr[l+1:]...)
-			l--
+			arr[k-counter-1] = arr[k-counter-1] + arr[k]
+			arr = append(arr[:k], arr[k+1:]...)
+			k--
 		}
 		counter = 0
 	}
-	//fmt.Println("==================================")
-	//for k := 0; k < len(arr); k++ {
-	//	fmt.Println(arr[k])
-	//}
 	return arr
 }
